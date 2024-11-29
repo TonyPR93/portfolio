7218ee84b1daa1ad6d4484d11f2c06afbbbb8024
Panont Anthony <anthonypanont@gmail.com>
2024-11-29 16:54:19 +0100

    Speed Insight

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n";
 import App from "./App.jsx";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectSpeedInsights();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
